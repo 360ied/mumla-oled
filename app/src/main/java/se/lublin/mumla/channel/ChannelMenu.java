57c021355b4cc03de50ff49e2b7f62b0cc0ae04f
360ied <19516527+360ied@users.noreply.github.com>
2026-08-15 22:42:20 -0400

    ui: hide channel listen action on active session channel

--- app/src/main/java/se/lublin/mumla/channel/ChannelMenu.java
+++ app/src/main/java/se/lublin/mumla/channel/ChannelMenu.java
@@ -93,9 +93,14 @@ public class ChannelMenu implements PermissionsPopupMenu.IOnMenuPrepareListener,
             }
             MenuItem listenItem = menu.findItem(R.id.context_channel_listen);
             if (listenItem != null) {
-                boolean isListening = mChannel.isListening();
-                listenItem.setChecked(isListening);
-                listenItem.setTitle(isListening ? R.string.context_channel_stop_listen : R.string.context_channel_listen);
+                if (ourChan != null && mChannel.getId() == ourChan.getId()) {
+                    listenItem.setVisible(false);
+                } else {
+                    listenItem.setVisible(true);
+                    boolean isListening = mChannel.isListening();
+                    listenItem.setChecked(isListening);
+                    listenItem.setTitle(isListening ? R.string.context_channel_stop_listen : R.string.context_channel_listen);
+                }
             }
         }
     }
